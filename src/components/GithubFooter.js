a93b03446a34aab085a4f6f8339a70fdc5ffd4ec
jopieji <jakeopie9@gmail.com>
2022-03-26 10:00:43 -0500

    final change to styles footer

--- src/components/GithubFooter.js
+++ src/components/GithubFooter.js
@@ -6,9 +6,9 @@ function GithubFooter() {
         <div className="footer">
             <Typography
                 variant="h6"
-                style={{color: "grey" }}
+                style={{color: "white" }}
             >
-            Github: <a href="https://github.com/jopieji" target="_blank">jopieji</a>
+            Github: <a href="https://github.com/jopieji" target="_blank" color="red">jopieji</a>
             </Typography>
         </div>
         
